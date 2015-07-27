daee6aed8b0fcbc1b6b8ec31dae1190ba22b3d5c
Jamie Rose <jamierose@Jamies-iMac.local>
2015-07-27 09:26:00 +0100

    updates some css and added twiter

--- primes.rb
+++ primes.rb
@@ -3,5 +3,13 @@ class Primes
 
     # TODO - add your prime number solution here...
     "I'm working on it!"
+    require 'prime'
+
+p = Prime.new
+(1..1000).each do |n|
+  q = p.next()
+  puts "#{n}: #{q}" 
+end
+Prime.each(1000).inject :+
     end
 end
